0df5e79b70136f99cff79f0e06500290ca6fabbd
Venki124 <venkat.venki124@gmail.com>
2025-10-28 18:05:54 +0530

    updated the code

--- main.py
+++ main.py
@@ -5,15 +5,62 @@
 # TODO - break out based on schema
 import base64
 from google.cloud import bigquery
+import json
 table_id = "neptune.rawmessages"
+processed_table_id = "neptune.processed_table"
+error_table_id = "neptune.error_table"
 
 
 def pubsub_to_bigquery(event, context):
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     print("Row To Insert: " + pubsub_message)
-    client = bigquery.Client()
-    table = client.get_table(table_id)
-    row_to_insert = [(pubsub_message,)]     # NOTE - the trailing comma is required for this case - it expects a tuple
-    errors = client.insert_rows(table, row_to_insert)
-    if errors == []:
-        print("Row Inserted: " + pubsub_message)
+
+    try:
+
+        client = bigquery.Client()
+        raw_table = client.get_table(table_id)
+        error_table=client.get_table(processed_table_id)
+        error_table=client.get_table(error_table_id)
+        row_to_insert = [(pubsub_message,)]     # NOTE - the trailing comma is required for this case - it expects a tuple
+        errors = client.insert_rows(raw_table, row_to_insert)
+
+        if errors == []:
+            print(f"Row successfully inserted: {row_to_insert}")
+        else:
+            print(f"BigQuery insert errors: {errors}")
+
+    except Exception as e:
+        print('failed to connect and get the table details')
+
+    # process the rows and insert
+    try:
+        jsonmessage = json.loads(pubsub_message)
+    except Exception as e:
+        print(f'json decode error:{e}')
+        #write to the pubsub message and error message to the bq
+        row_to_insert = [(pubsub_message,e)]
+        erros = client.insert_rows(error_table,row_to_insert)
+
+        if errors == []:
+            print(f"Error Row successfully inserted: {row_to_insert}")
+        else:
+            print(f"BigQuery insert errors: {errors}")
+
+    row_to_insert = [{
+            "id": jsonmessage.get("id"),
+            "ipaddress": jsonmessage.get("ipaddress"),
+            "action": jsonmessage.get("action"),
+            "accountnumber": jsonmessage.get("accountnumber"),
+            "actionid": jsonmessage.get("actionid"),
+            "name": jsonmessage.get("name"),
+            "actionby": jsonmessage.get("actionby")
+        }]
+    
+    try:
+        errors = client.insert_rows_json(table_id, row_to_insert)
+        if errors == []:
+            print(f"Row successfully inserted: {row_to_insert}")
+        else:
+            print(f"BigQuery insert errors: {errors}")
+    except Exception as e:
+        print(f"BigQuery exception: {e}")
---
+++ prep-project.sh
@@ -0,0 +1,35 @@
+# create the neptune dataset and table
+bq mk neptune
+
+bq mk --table --schema message:string $GOOGLE_CLOUD_PROJECT:neptune.rawmessages
+bq mk --table --schema id:string,ipaddress:string,action:string,accountnumber:string,actionid:integer,name:string,actionby:string $GOOGLE_CLOUD_PROJECT:neptune.processed_table
+bq mk --table --schema message:string,error:string $GOOGLE_CLOUD_PROJECT:neptune.error_table
+
+# enable event ark and pubsub api
+gcloud services enable eventarc.googleapis.com
+gcloud services enable pubsub.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable cloudfunctions.googleapis.com
+
+
+<<COMMENT_BLOCK
+# create the pubsub topics
+gcloud pubsub topics create neptune-activites
+
+# create the pubsub subscription
+gcloud pubsub subscriptions create neptune-activites-test --topic=neptune-activites
+
+#publish a message to topic
+gcloud pubsub topics publish neptune-activites --message='Hello World!'
+
+#example json message
+gcloud pubsub topics publish neptune-activities \
+  --message='{"id":"20200812040801981475","ipaddress":"195.174.170.81","action":"UPDATE","accountnumber":"GB25BZMX47593824219489","actionid":4,"name":"Emily Blair","actionby":"STAFF"}'
+
+# create the cloud functions
+gcloud functions deploy function_pb_bq --gen2 --region=us-central1 --runtime=python312 --trigger-topic=neptune-activites --entry-point=pubsub_to_bigquery --memory=256MB
+
+# list the cloud functions
+gcloud functions list
+
+COMMENT_BLOCK
