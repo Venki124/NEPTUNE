e152d8c2696a8f7fe1b324a860dd8165d8744951
Venki124 <venkat.venki124@gmail.com>
2025-10-29 21:04:17 +0530

    updated the code

--- run_cloud_fun.sh
+++ run_cloud_fun.sh
@@ -5,4 +5,6 @@ gcloud functions deploy function_pb_bq --gen2 \
     --region=us-central1 --runtime=python312 \
     --trigger-topic=neptune-activities \
     --entry-point=pubsub_to_bigquery \
-    --memory=256MB
+    --memory=256MB \
+    --service-account=$(gcloud iam service-accounts list --filter="EMAIL  ~ compute" --format='value(EMAIL)') \
+    --run-service-account=$(gcloud iam service-accounts list --filter="EMAIL  ~ compute" --format='value(EMAIL)')
